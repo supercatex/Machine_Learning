a858e8e23ee681eeb8db4a4253761d8269c504b5
Kinda <lamkinun@gmail.com>
2019-04-29 14:17:09 +0800

    mnist

--- Ex04.py
+++ Ex04.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import random
+import json
 import numpy as np
 from keras.models import Sequential
 from keras import layers
@@ -28,20 +29,48 @@ while True:
         break
 cv.destroyAllWindows()
 
-model = Sequential()
-model.add(layers.Conv2D(
-    input_shape=(28*28),
-    units=128,
-    activation=activations.relu
-))
-model.add(layers.Dense(10, activation="softmax"))
-model.compile(optimizer=optimizers.Adam(), loss=losses.categorical_crossentropy)
-
-X_train = X_train.reshape(len(X_train), 28*28)
+X_train = X_train.reshape(len(X_train), 28, 28, 1)
+print(X_train.shape)
 X_train = X_train.astype('float32')
 X_train /= 255
 Y_train = np_utils.to_categorical(y_train, 10)
-model.fit(X_train, Y_train, epochs=10, batch_size=32)
+
+model = Sequential()
+model.add(layers.Conv2D(
+    input_shape=(28, 28, 1),
+    filters=64,
+    kernel_size=(5, 5),
+    activation=activations.relu
+))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(layers.Conv2D(
+    filters=128,
+    kernel_size=(3, 3),
+    activation=activations.relu
+))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(layers.Conv2D(
+    filters=128,
+    kernel_size=(3, 3),
+    activation=activations.relu
+))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(1024, activation=activations.relu))
+model.add(layers.Dense(512, activation=activations.relu))
+model.add(layers.Dense(10, activation=activations.softmax))
+model.compile(optimizer=optimizers.Adam(), loss=losses.categorical_crossentropy)
+
+if os.path.exists("my_model.h5"):
+    model.load_weights("my_model.h5")
+else:
+    model.fit(X_train, Y_train, epochs=10, batch_size=32)
+
+    model.save_weights("my_model.h5")
+    f = open("my_model.json", "w")
+    f.write(model.to_json())
+    f.close()
+
 
 is_drawing = False
 def onmouse_image(event, x, y, flags, param):
@@ -68,7 +97,7 @@ while True:
     if key == 32:
         tmp = img.copy()
         tmp = cv.resize(tmp, (28, 28))
-        tmp = tmp.reshape(1, 28 * 28)
+        tmp = tmp.reshape(1, 28, 28, 1)
         tmp = tmp.astype('float32')
         tmp /= 255
         predict = model.predict(tmp)
